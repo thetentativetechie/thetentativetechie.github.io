666d0484f51803002b2b166eb44c4127ff074f81
thetentativetechie <umeshkumark@outlook.com>
2017-09-19 22:35:55 +0530

    added animations

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -36,6 +37,7 @@ import {CustomMaterialModule} from './common/material.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     CustomRoutingModule,
     CustomMaterialModule,
--- app/common/component/imagechip.component.ts
+++ app/common/component/imagechip.component.ts
@@ -1,13 +1,50 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, trigger, state, style, transition, animate, keyframes} from '@angular/core';
 import {ImageChip} from '../model/imagechip.model';
 
 @Component({
     selector: 'image-chip',
     styleUrls: ['./imagechip.component.css'],
-    templateUrl: './imagechip.component.html'
+    templateUrl: './imagechip.component.html',
+    animations: [
+        trigger ('fadein', [
+            state('0', style({
+                transform: 'translateX(-100%)'
+            })),
+            state('1', style({
+                transform: 'translateX(100%)'
+            })),
+            transition('0 => 1', [
+                animate(300, keyframes([
+                style({opacity: 0, transform: 'translateX(-50%)', offset: 0}),
+                style({opacity: 1, transform: 'translateX(15px)',  offset: 0.3}),
+                style({opacity: 1, transform: 'translateX(0)',     offset: 1.0})
+                ]))
+            ]),
+            transition('1 => 0', [
+                animate(300, keyframes([
+                  style({opacity: 1, transform: 'translateX(0)',     offset: 0}),
+                  style({opacity: 1, transform: 'translateX(-15px)', offset: 0.7}),
+                  style({opacity: 0, transform: 'translateX(90%)',  offset: 1.0})
+                ]))
+            ])
+        ])
+    ]
 })
 export class ImageChipComponent {
 
+    state: number = 0;
+
     @Input() imagechip: ImageChip;
+
+    constructor() {
+        setTimeout(() => {
+            if (this.state === 0) {
+                this.state = 1;
+            } else {
+                this.state = 0;
+            }
+            console.log(this.state);
+        }, 1000);
+    }
 
 }
